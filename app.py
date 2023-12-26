50a74f77669681772cd78fd48ea7fe96c12fdade
zhenyang1999 <71130436+zhenyang1999@users.noreply.github.com>
2023-12-26 17:45:48 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+import base64
+from io import BytesIO
+
+def main():
+    st.title("Excel Report Generator")
+
+    # File upload
+    uploaded_file = st.file_uploader("Upload an Excel file", type=["xls", "xlsx"])
+
+    if uploaded_file is not None:
+        # Read Excel file into DataFrame
+        df = pd.read_excel(uploaded_file)
+        
+        # Display uploaded data
+        st.subheader("Uploaded Data:")
+        st.dataframe(df)
+
+        df['add_hundred'] = df.iloc[:,0] + 100
+        df['substract_hundred'] = df.iloc[:,0] - 100
+        df['multiply_hundred'] = df.iloc[:,0] * 100
+        df['divided_hundred'] = df.iloc[:,0] / 100
+        
+        # Generate a simple report
+        st.subheader("Generated Report:")
+        report_data = df.head()  # You can customize this part based on your requirements
+        st.dataframe(report_data)
+
+        # Export button to download the report as Excel
+        st.subheader("Export Report:")
+        export_button = st.button("Export Report as Excel")
+        if export_button:
+            export_filename = "generated_report.xlsx"
+            st.markdown(get_table_download_link(report_data, export_filename), unsafe_allow_html=True)
+
+def get_table_download_link(df, filename):
+    """Generate a download link for the DataFrame as an Excel file."""
+    excel_buffer = BytesIO()
+    df.to_excel(excel_buffer, index=False, header=True, engine="openpyxl")
+    excel_data = excel_buffer.getvalue()
+    excel_buffer.close()
+    b64 = base64.b64encode(excel_data).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="{filename}">Excel</a>'
+    return href
+
+if __name__ == "__main__":
+    main()
